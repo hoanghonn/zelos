8e0e0b4508a4e3de8c1d0fbd40ef327398d5275f
hoanghonn <hoang.nguyen@advancegroup.comwq>
2025-04-03 22:17:51 +0700

    Update details page

--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -1,3 +1,6 @@
 class Hackathon < ApplicationRecord
   has_one_attached :image
+  after_commit -> { broadcast_refresh_later_to "hackathons" }
+
+  validates :title, :description, :image, presense: true
 end
---
+++ config/initializers/active_record.rb
@@ -0,0 +1,5 @@
+Rails.application.config.after_initialize do
+  ActiveStorage::Current.url_options = {
+    host: "http://localhost:3000"
+  }
+end
